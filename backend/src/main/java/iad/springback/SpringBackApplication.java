004f6e33ca81664c3669e3790d9b33ac09f68019
YTitan <tcherezovyuri@mail.ru>
2019-01-30 19:36:35 +0300

    Initial

---
+++ backend/src/main/java/iad/springback/SpringBackApplication.java
@@ -0,0 +1,14 @@
+package iad.springback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackApplication.class, args);
+	}
+
+}
+
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+
+      <div class="webgl-content">
+          <div id="gameContainer" style="width: 960px; height: 600px"></div>
+          <div class="footer">
+              <div class="webgl-logo"></div>
+              <div class="fullscreen" v-on:click="onClick"></div>
+              <div class="title">AlcheMix</div>
+          </div>
+      </div>
+
+      <!--<unity src="Build/webgl.json" unity-loader="Build/UnityLoader.js" height="600" width="900"></unity>-->
+  </div>
+</template>
+
+
+<script>
+
+    export default {
+      name: 'HelloWorld',
+      props: {
+        msg: String,
+      },
+        gameInstance: {},
+      mounted: function () {
+          this.$nextTick(function () {
+              this.gameInstance = UnityLoader.instantiate("gameContainer", "Build/webgl.json", {onProgress: UnityProgress});
+          })
+      },
+        methods: {
+          onClick(){
+              this.gameInstance.SetFullscreen(1);
+          }
+        }
+    }
+</script>
+
+<style src="../../public/TemplateData/style.css"></style>
+
