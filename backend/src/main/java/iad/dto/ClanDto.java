1bc559f2d332742d03b57e8c341c16fd88bb32d8
YTitan <tcherezovyuri@mail.ru>
2019-02-24 19:17:13 +0300

    Clan updates

--- backend/src/main/java/iad/dto/ClanDto.java
+++ backend/src/main/java/iad/dto/ClanDto.java
@@ -9,4 +9,28 @@ public class ClanDto implements Serializable {
     private String description;
 
     private long imageId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(long imageId) {
+        this.imageId = imageId;
+    }
 }
